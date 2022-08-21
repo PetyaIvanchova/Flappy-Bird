00b1368946fbe22cdeb12049507ca272540965dd
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-08-21 12:45:49 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace igra1
+{
+    public partial class Form1 : Form
+    {
+
+        int pipeSpeed = 8;
+        int gravity = 15;
+        int score = 0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HappyBirrd_Click(object sender, EventArgs e)
+        {   
+            
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            HappyBirrd.Top += gravity;
+            pipeBotton.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed;
+            scoreText.Text = "Score:" + score ;
+            
+            if (pipeBotton.Left<-150)
+            {
+                pipeBotton.Left = 800;
+                score++;
+            }
+            if (pipeTop.Left<-180)
+            {
+                pipeTop.Left = 950;
+                score++;
+            }
+            if (HappyBirrd.Bounds.IntersectsWith(pipeBotton.Bounds) || HappyBirrd.Bounds.IntersectsWith(pipeTop.Bounds) || HappyBirrd.Bounds.IntersectsWith(ground.Bounds) || (HappyBirrd.Top < -25))
+            {
+                endGame();
+            }
+            if (score> 5)
+            {
+                pipeSpeed = 15;
+            }
+            
+        }
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Space)
+            {
+                gravity = -15;
+            }
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 15;
+            }
+
+
+        }
+
+        private void endGame()
+        {
+            gameTimer.Stop();
+            scoreText.Text += "  Game over!!!";
+        }
+
+    }
+
+
+}
